c7a50dbd219248191cbbbb4a1a9a59f052645977
LeeSungKeun <draw.git@gmail.com>
2020-05-20 23:27:56 +0900

    UImage 공통API 추가 (사진 리사이즈 메서드추가)

--- Draw_Demo/common/common.swift
+++ Draw_Demo/common/common.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+import UIKit
 extension Dictionary {
     var queryString : String {
         var output = ""
@@ -23,5 +23,26 @@ extension Dictionary {
 extension Notification.Name {
     static let signUp = Notification.Name(rawValue: "signUpNotification")
 }
+
+
+extension UIImage {
+    func fixOrigentaion() -> UIImage {
+        //사진이 up상태에서 찍엇을경우는 리턴
+        if imageOrientation == .up {
+            return self
+        }
+        // 그외 사진은 다시그린다.
+        UIGraphicsBeginImageContextWithOptions(size, true, scale)
+        draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+
+        if let nomalizedImage = UIGraphicsGetImageFromCurrentImageContext() {
+            UIGraphicsEndImageContext()
+            return nomalizedImage
+        } else {
+            return self
+        }
+    }
+}
+
 
 
