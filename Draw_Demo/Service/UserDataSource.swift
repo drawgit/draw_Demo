6c00475eb30071148e3b702da2fc2ebe1bd03f52
LeeSungKeun <draw.git@gmail.com>
2020-06-05 00:15:39 +0900

    소스정리

---
+++ Draw_Demo/Service/UserDataSource.swift
@@ -0,0 +1,21 @@
+//
+//  UserDataSource.swift
+//  Draw_Demo
+//
+//  Created by 이성근 on 26/05/2020.
+//  Copyright © 2020 Draw_corp. All rights reserved.
+//
+
+import Foundation
+
+class UserDataSource {
+
+    static let shared = UserDataSource()
+
+    private init(){
+    }
+
+
+
+
+}
